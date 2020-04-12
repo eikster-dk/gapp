a74f55d4ed47e45933dfb9dc5ba390ce0966003b
Eik Emil Christensen <me@eikc.dk>
2020-04-12 20:43:27 +0200

    extracted into a few methods

--- internal/secrets/management.go
+++ internal/secrets/management.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"github.com/eikc/gapp/internal/gh"
-	"github.com/eikc/gapp/internal/ux"
 	"gopkg.in/yaml.v2"
 	"strings"
 )
@@ -24,30 +23,20 @@ type ManagementParams struct {
 }
 
 func (cli *CLI) RunManagement(ctx context.Context, params ManagementParams) error {
-	spinner, err := ux.NewSpinner("Creating or Updating secrets", "Reading file")
+	sortedSecrets, err := cli.parseAndSort(params)
 	if err != nil {
 		return err
 	}
 
-	spinner.Start()
-
-	content, err := cli.reader.ReadFile(params.File)
+	err = cli.updateSecrets(ctx, sortedSecrets)
 	if err != nil {
-		spinner.Fail()
-
 		return err
 	}
 
-	var secrets Secrets
-	err = yaml.Unmarshal(content, &secrets)
-	if err != nil {
-		spinner.Fail()
+	return nil
+}
 
-		return err
-	}
-
-	sortedSecrets := sortSecrets(secrets)
-
+func (cli *CLI) updateSecrets(ctx context.Context, sortedSecrets map[string][]Secret) error {
 	for repo, secrets := range sortedSecrets {
 		splitted := strings.Split(repo, "/")
 		if len(splitted) < 2 {
@@ -59,18 +48,12 @@ func (cli *CLI) RunManagement(ctx context.Context, params ManagementParams) erro
 		repo := splitted[1]
 		pkey, pkeyId, err := cli.githubClient.GetPublicKey(ctx, owner, repo)
 		if err != nil {
-			spinner.Fail()
-
 			return err
 		}
 
 		for _, secret := range secrets {
-			spinner.Message(fmt.Sprintf("%s for repo %s", secret.Name, repo))
-
 			encoded, err := cli.encryptionWriter.Encrypt(secret.Value, pkey)
 			if err != nil {
-				spinner.Fail()
-
 				return err
 			}
 
@@ -81,14 +64,28 @@ func (cli *CLI) RunManagement(ctx context.Context, params ManagementParams) erro
 			})
 
 			if err != nil {
-				spinner.Fail()
-
 				return err
 			}
 		}
 	}
 
-	return spinner.Stop()
+	return nil
+}
+
+func (cli *CLI) parseAndSort(params ManagementParams) (map[string][]Secret, error) {
+	content, err := cli.reader.ReadFile(params.File)
+	if err != nil {
+		return nil, err
+	}
+
+	var secrets Secrets
+	err = yaml.Unmarshal(content, &secrets)
+	if err != nil {
+		return nil, err
+	}
+
+	sortedSecrets := sortSecrets(secrets)
+	return sortedSecrets, nil
 }
 
 func sortSecrets(secrets Secrets) map[string][]Secret {

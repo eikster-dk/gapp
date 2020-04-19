c71a79c84549830b8b246941850c5dee75c4e8f8
Eik Emil Christensen <me@eikc.dk>
2020-04-19 18:28:17 +0200

    added tests to files pkg

--- internal/files/files.go
+++ internal/files/files.go
@@ -3,9 +3,14 @@ package files
 import (
 	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
-type Reader struct {}
+type Reader struct{}
+
+type ReadFile interface {
+	ReadFile(path string) ([]byte, error)
+}
 
 func (f Reader) IsFile(path string) (bool, error) {
 	fi, err := os.Stat(path)
@@ -23,6 +28,16 @@ func (f Reader) ReadFile(path string) ([]byte, error) {
 	return ioutil.ReadFile(path)
 }
 
-func (f Reader) ReadDir(path string) ([]os.FileInfo, error) {
-	return ioutil.ReadDir(path)
+func (f Reader) ReadDir(path string) ([]string, error) {
+	ff, err := ioutil.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var paths []string
+	for _, f := range ff {
+		paths = append(paths, filepath.Join(path, f.Name()))
+	}
+
+	return paths, nil
 }
